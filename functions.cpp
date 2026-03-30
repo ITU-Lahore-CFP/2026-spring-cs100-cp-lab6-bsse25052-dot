2fea8d50dd92b585d261c44ce67197a67e28ed5e
bsse25052 <bsse25052@itu.edu.pk>
2026-03-30 06:54:11 +0500

    LAB 6 DONE
    LAB 6 DONE

--- functions.cpp
+++ functions.cpp
@@ -1,14 +1,24 @@
 #include "functions.h"
 
 
-int sumOfSquares(int n) 
-{
+int sumOfSquares(int n) {
+    // Base case
+    if (n <= 1)
+    {
+        return n;
+    }
+
+    // Recursive case
+    return (n * n) + sumOfSquares(n - 1);
 }
 
-int findFactorial(int n)
-{
+int findFactorial(int n) {
+    // Base case
+    if (n <= 1)
+    {
+        return 1;
+    }
+
+    // Recursive case
+    return n * findFactorial(n - 1);
 }
-
-
-
-
--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include "functions.h"
+#include "functions.cpp"
 
 
 int main() {
-   
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Sum of squares: " << sumOfSquares(n) << endl;
+    cout << "Factorial of the number is " << findFactorial(n) << endl;
+
+    return 0;
 }
